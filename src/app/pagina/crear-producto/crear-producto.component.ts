39f006afb5765e0902839737b2da107441fa882a
Nicolás Jaramillo Arcos <nicolas.jaramilloa@uqvirtual.edu.co>
2023-05-07 16:15:45 -0500

    Se avanzó hasta el punto 18

--- src/app/pagina/crear-producto/crear-producto.component.ts
+++ src/app/pagina/crear-producto/crear-producto.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-crear-producto',
   templateUrl: './crear-producto.component.html',
   styleUrls: ['./crear-producto.component.css']
 })
-export class CrearProductoComponent {
+export class CrearProductoComponent implements OnInit{
+
+    onFileChange(event:any){
+    if (event.target.files.length > 0) {
+    const files = event.target.files;
+    console.log(files);
+    }
+    }
+
+      categorias:string[];
+    constructor(){
+    this.categorias = [];
+    }
+
+      ngOnInit(): void {
+        this.categorias.push('Tecnología');
+        this.categorias.push('Hogar');
+        this.categorias.push('Deportes');
+        this.categorias.push('Moda');
+        this.categorias.push('Mascotas');
+      }
 
 }
